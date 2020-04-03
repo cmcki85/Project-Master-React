5f0b0c2b775ef3e34f5d8a39cc607236fef8c793
Cameron <cameronsmowing@gmail.com>
2020-04-03 17:03:02 +1000

    Reverted back to class based root component and passed methods through props to additional components.

--- 2. Basic React Syntax + Features/react-complete/src/App.js
+++ 2. Basic React Syntax + Features/react-complete/src/App.js
@@ -1,35 +1,45 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Components/Person/person';
 
-const app = (props) => {
-	const [personsState, setPersonsState] = useState({
+class App extends Component {
+	state = {
 		persons: [
 			{name: 'Cam', age: '19'},
 			{name: 'Lauren', age: '17'}
 		]
-	})
-	const [otherState, setOtherState] = useState('Some other value');
+	}
 
-	const switchNameHandler = () => {
-		setPersonsState({
+	switchNameHandler = (newName) => {
+		// console.log("Click!")
+		this.setState({
 			persons: [
-				{name: 'Cameron', age: '20'},
+				{name: newName, age: '20'},
 				{name: 'Lauren', age: '17'}
 			],
 			otherState: 'Some Value'
 		})
 	}
 
-	return (
-		<div className='App'>
-			<h1>Is this working?</h1>
-			<button onClick = {switchNameHandler} >Switch Name</button>
-			<Person name={personsState.persons[0].name} age={personsState.persons[0].age}> My Hobbies: Programming </Person>
-			<Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
-			
-		</div>
-	);
+	render() {
+		return (
+			<div className='App'>
+				<h1>Is this working?</h1>
+				<button onClick = {this.switchNameHandler.bind(this, 'Cameron')} >Switch Name</button>
+				<Person 
+					name={this.state.persons[0].name} 
+					age={this.state.persons[0].age}>
+						My Hobbies: Programming 
+				</Person>
+				<Person 
+					name={this.state.persons[1].name} 
+					age={this.state.persons[1].age}
+					click = {this.switchNameHandler.bind(this, 'Cam!!')}
+				/>
+				
+			</div>
+		);
+	}
 }
 
-export default app;
+export default App;
--- 2. Basic React Syntax + Features/react-complete/src/Components/Person/person.js
+++ 2. Basic React Syntax + Features/react-complete/src/Components/Person/person.js
@@ -4,7 +4,7 @@ import React from 'react';
 const person = ( props ) => {
     return (
         <div>
-            <p>I am {props.name} and i am {props.age}. </p>
+            <p onClick={props.click}>I am {props.name} and i am {props.age}. </p>
             <p>{props.children}</p>
         </div>
         
