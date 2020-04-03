5ae089740156ce418e1b631b8936e66e44ff2ff2
Cameron <cameronsmowing@gmail.com>
2020-04-03 13:21:35 +1000

    Basic in-line JS was added and used inside a single Functional Component

--- 2. Basic React Syntax + Features/react-complete/src/App.js
+++ 2. Basic React Syntax + Features/react-complete/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Components/Person/person';
 
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
 				<h1>Hello</h1>
+				<Person />
+				<Person />
+				<Person />
 			</div>
 		);
 	}
---
+++ 2. Basic React Syntax + Features/react-complete/src/Components/Person/person.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Functional Person component 
+const person = () => {
+    return (
+        <p>This is a person and i am {Math.floor(Math.random() * 30)} years old!</p>
+    )
+}
+
+export default person;
