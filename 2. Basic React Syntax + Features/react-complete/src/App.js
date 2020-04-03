c2e68ab70c681dca6f48933db999a6cf92ef82d6
Cameron <cameronsmowing@gmail.com>
2020-04-03 13:52:14 +1000

    Simple event handler to update state of class based components added.

--- 2. Basic React Syntax + Features/react-complete/src/App.js
+++ 2. Basic React Syntax + Features/react-complete/src/App.js
@@ -3,12 +3,31 @@ import './App.css';
 import Person from './Components/Person/person';
 
 class App extends Component {
+	state = {
+		persons: [
+			{name: 'Cam', age: '19'},
+			{name: 'Lauren', age: '17'}
+		]
+	}
+
+	switchNameHandler = (event) => {
+		// console.log("Click!")
+		this.setState({
+			persons: [
+				{name: 'Cameron', age: '20'},
+				{name: 'Lauren', age: '17'}
+			],
+			otherState: 'Some Value'
+		})
+	}
+
 	render() {
 		return (
 			<div className='App'>
-				<h1>Hello</h1>
-				<Person name='Cam' age='19'/>
-				<Person name='Lauren' age='17'/>
+				<h1>Is this working?</h1>
+				<button onClick = {this.switchNameHandler} >Switch Name</button>
+				<Person name={this.state.persons[0].name} age={this.state.persons[0].age}> My Hobbies: Programming </Person>
+				<Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
 				
 			</div>
 		);
--- 2. Basic React Syntax + Features/react-complete/src/Components/Person/person.js
+++ 2. Basic React Syntax + Features/react-complete/src/Components/Person/person.js
@@ -3,7 +3,11 @@ import React from 'react';
 // Functional Person component 
 const person = ( props ) => {
     return (
-        <p>I am {props.name} and i am {props.age}. </p>
+        <div>
+            <p>I am {props.name} and i am {props.age}. </p>
+            <p>{props.children}</p>
+        </div>
+        
     )
 }
 
