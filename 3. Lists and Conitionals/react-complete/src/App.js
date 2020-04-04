fb59b47ee0f0fd2a9c565e49815dd0bca1e6da7c
Cameron <cameronsmowing@gmail.com>
2020-04-04 13:35:30 +1000

    More effecient conditional rendering added.

--- 3. Lists and Conitionals/react-complete/src/App.js
+++ 3. Lists and Conitionals/react-complete/src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
 		persons: [
 			{name: 'Cam', age: '19'},
 			{name: 'Lauren', age: '17'}
-		]
+		],
+		otherState: 'Some Value',
+		showPersons: false,
 	}
 	
 	switchNameHandler = (newName) => {
@@ -28,8 +30,7 @@ class App extends Component {
 				{name: 'Cameron', age: '20'},
 				{name: event.target.value, age: '17'}
 			],
-			otherState: 'Some Value',
-			showPersons: false,
+			otherState: 'Some Value'
 		})
 	}
 
@@ -41,27 +42,31 @@ class App extends Component {
 	}
 
 	render() {
+
+		let persons = null;
+		if (this.state.showPersons) {
+			persons = (
+				<div>
+					<Person 
+						name={this.state.persons[0].name} 
+						age={this.state.persons[0].age}>
+							My Hobbies: Programming 
+					</Person>
+					<Person 
+						name={this.state.persons[1].name} 
+						age={this.state.persons[1].age}
+						click = {this.switchNameHandler.bind(this, 'Cam!!')}
+						changed = {this.handleNameChange}
+					/>
+				</div>
+			)
+		}
+
 		return (
 			<div className='App'>
 				<h1>Change these names:</h1>
 				<button onClick = {this.togglePersonHandler} >Toggle Names</button>
-				{ this.state.showPersons ? 
-					<div>
-						<Person 
-							name={this.state.persons[0].name} 
-							age={this.state.persons[0].age}>
-								My Hobbies: Programming 
-						</Person>
-						<Person 
-							name={this.state.persons[1].name} 
-							age={this.state.persons[1].age}
-							click = {this.switchNameHandler.bind(this, 'Cam!!')}
-							changed = {this.handleNameChange}
-						/>
-					</div> : null
-				}
-				
-
+				{ persons } 
 			</div>
 		);
 	}
